3d6c2a2af9d7813c941c8410ebf23a1225eb746d
Pobitrodeb <pobitrodn@gmail.com>
2023-01-31 21:51:44 +0600

    successfully done VueJs routing sessions

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,6 +1,7 @@
 <template>
     <h1> Blog Content From Blog Router  </h1>
-   <b> Blog ID  {{ this.$route.params.id}}</b>
+   <b> Blog ID  {{ this.$route.params.id}}</b> 
+   <!-- params->id means , your dynamic params  -->
 </template>
 
 <script>
