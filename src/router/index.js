98f35b983ecd2af4a190f86a6201cdfb6a6cd14b
Pobitrodeb <pobitrodn@gmail.com>
2023-01-31 21:21:49 +0600

    about page routing successfully done

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
         name: "home", 
         component: () => import('../components/Home.vue'), 
     }, 
+    {
+        path: "/about", 
+        name: "about", 
+        component: () => import('../components/About.vue'), 
+    }, 
 ]
 export default  createRouter({
     history:createWebHistory('/'),
